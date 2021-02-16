dd78e99f6aa05064f9bd2206a495f6bbe0c94427
Patrik Radics <54003031+Codethier@users.noreply.github.com>
2021-02-16 10:42:54 +0100

    in

--- src/app/heroes-component/heroes-component.component.ts
+++ src/app/heroes-component/heroes-component.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Heroes} from './heroes';
+import {HeroesService} from '../services/hero-feed.service';
 
 @Component({
   selector: 'app-heroes-component',
@@ -6,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes-component.component.scss']
 })
 export class HeroesComponentComponent implements OnInit {
+  heroesList: Heroes[] = [];
 
-  constructor() { }
+  constructor(heroesService: HeroesService) {
+    this.heroesList = heroesService.getAll();
+  }
 
   ngOnInit(): void {
   }
