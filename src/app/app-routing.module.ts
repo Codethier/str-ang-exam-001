9b3303e6f96f961aa1236541d31fb14617c42f1d
Patrik Radics <54003031+Codethier@users.noreply.github.com>
2021-02-16 10:16:05 +0100

    in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponentComponent} from './home-component/home-component.component';
+import {HeroesComponentComponent} from './heroes-component/heroes-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponentComponent,
+  },
+  {
+    path: 'heroes',
+    component: HeroesComponentComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '', pathMatch: 'full'
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,21 @@ import { FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import {RouterModule} from '@angular/router';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { HeroesComponentComponent } from './heroes-component/heroes-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    HomeComponentComponent,
+    HeroesComponentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
